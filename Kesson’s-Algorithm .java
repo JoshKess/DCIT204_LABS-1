ba268e51c7a6c09da2b4e92f3f6ea6182459a435
JoshKess <joshuakesson987@gmail.com>
2026-06-17 11:05:53 +0000

    Add files via upload
    Algorithm.java -22356528

---
+++ Kesson’s-Algorithm .java
@@ -0,0 +1,31 @@
+public class Algorithms {
+
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
